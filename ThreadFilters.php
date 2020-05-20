85d998300cee88dbed649f507227138aa86f81ce
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-05-20 11:43:59 -0400

    Update ThreadFilters.php

--- ThreadFilters.php
+++ ThreadFilters.php
@@ -10,13 +10,20 @@ class ThreadFilters extends Filters
     
   protected $filters = ['by', 'popular', 'unanswered'];
   
+     /*
+    Filter the query by a given username
+    */
+    
   protected function by($username)
   {
             $user = User::where('name', $username)->firstOrFail();
             
             return $this->builder->where('user_id', $user->id);
         }
-        
+         /*
+    Filter the query by most popular threads
+    */
+    
     protected function popular() {
         $this->builder->getQuery()->orders = [];
        return $this->builder->orderBy('replies_count', 'desc');
