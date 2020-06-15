535ca6fe5298dabfec3015c7aa998712846379e1
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 06:31:48 -0400

    Update Favorite.php

--- Models/Favorite.php
+++ Models/Favorite.php
@@ -5,11 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
+    
 {
     use RecordsActivity;
     protected $guarded  = [];
     
-    public function favorited(){
+    public function favorited()
+    
+    {
         return $this->morphTo();
     }
     
