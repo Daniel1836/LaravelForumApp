d601d03f18bf26efdebfaf57af6d93614f777c29
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 06:31:18 -0400

    Update Channel.php

--- Models/Channel.php
+++ Models/Channel.php
@@ -5,14 +5,17 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
+    
 {
     
     public function getRouteKeyName()
+        
     {
         return 'slug';
     }
     
     public function threads()
+        
     {
         return $this->hasMany(Thread::class);
     }
