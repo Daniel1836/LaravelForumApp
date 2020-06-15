d4724bb30f66e08f08ecad28f18c01ac485f0338
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 06:29:07 -0400

    Update create_replies_table.php

--- Migrations/create_replies_table.php
+++ Migrations/create_replies_table.php
@@ -5,15 +5,19 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateRepliesTable extends Migration
+    
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
+    
     public function up()
+        
     {
-        Schema::create('replies', function (Blueprint $table) {
+        Schema::create('replies', function (Blueprint $table)
+        {
             $table->increments('id');
             $table->integer('thread_id');
             $table->integer('user_id');
@@ -28,6 +32,7 @@ class CreateRepliesTable extends Migration
      * @return void
      */
     public function down()
+        
     {
         Schema::dropIfExists('articles');
     }
