79f2eb471fbf0eb646397432348e630d56d3e0ca
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 06:46:49 -0400

    Update created_reply.blade.php

--- Views/created_reply.blade.php
+++ Views/created_reply.blade.php
@@ -5,11 +5,9 @@
                             {{ $profileUser->name }} replied to
                             <a href"{{ $activity->subject->thread->path() }}">"{{ $activity->subject->thread->title }}"</a>
                          </span>
-                       
                      </div>
-                     
                  </div>
-                 <div class="panel-body">
+                   <div class="panel-body">
                        {{ $activity->subject->body }}
-                 </div>
+                   </div>
              </div>
