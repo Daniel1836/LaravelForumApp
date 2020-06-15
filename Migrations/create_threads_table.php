bdc17eab3fa56a3e604c3ca07519f3cc0fa523e3
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 06:29:59 -0400

    Update create_threads_table.php

--- Migrations/create_threads_table.php
+++ Migrations/create_threads_table.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateThreadsTable extends Migration
+    
 {
     /**
      * Run the migrations.
@@ -12,8 +13,10 @@ class CreateThreadsTable extends Migration
      * @return void
      */
     public function up()
+        
     {
-        Schema::create('threads', function (Blueprint $table) {
+        Schema::create('threads', function (Blueprint $table)
+        {
             $table->increments('id');
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('channel_id');
@@ -29,8 +32,10 @@ class CreateThreadsTable extends Migration
      *
      * @return void
      */
+    
     public function down()
+        
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('');
     }
 }
