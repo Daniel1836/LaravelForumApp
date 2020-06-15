de466a1ccbba2c40007a2bd392d82f5f3c09e206
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 06:55:05 -0400

    Update show.blade.php

--- Views/show.blade.php
+++ Views/show.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
-
- <body>
+   <body>
        <div class="navdiv">
             <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top">
                 <button class="navbar-toggler mr-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,7 +14,7 @@
        <div class="container">
                 <div class="row thread">
                     <div class="col-md-12">
-                    <div class="col-md-8">
+                      <div class="col-md-8">
                         <div class="panel panel-default">
                             
                            <div class="creator">
@@ -39,21 +38,21 @@
                         <div id="reply-{{ $reply->id }}" class="panel panel-default>
                             <div class="panel-heading">
                               <div class="time">
-                              <div class="level">
-                              <h5 class="flex">
-                              <a href="/profiles/{{ $reply->owner->name }}">
-                             {{$reply->owner->name}} </a> said {{ $reply->created_at->diffforHumans() }}...
-                             </h5>
-                             <div>
+                                <div class="level">
+                                  <h5 class="flex">
+                                    <a href="/profiles/{{ $reply->owner->name }}">
+                                     {{$reply->owner->name}} </a> said {{ $reply->created_at->diffforHumans() }}...
+                                  </h5>
+                               <div>
                              <form method="POST" action="/replies/{{ $reply->id }}/favorites">
                              {{ csrf_field() }}
                              <div class="fav">
                                <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled':'' }}>{{ $reply->favorites()->count()}} {{ str_plural('Favorite', $reply->favorites()->count()) }}</button>
-                               </div>
-                             </form>
                              </div>
+                             </form>
+                                </div>
                               </div>
-                              </div>
+                           </div>
                            <hr>
                              <div class="panel-body">{{ $reply -> body }}
                              </div>
@@ -91,14 +90,11 @@
                          <div class="panel panel-default">
                               <div class="panel-body">
                                   <p>This thread was published {{ $thread->created_at->diffForHumans() }} by <a href="#">{{ $thread->creator->name }}</a>, and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.</p>
-                                  </div>
-                             
-                            </div>
+                              </div>
+                          </div>
                     </div>
                 </div>
             </div>
-                
-               
         </div>
 </body>
 </html>
