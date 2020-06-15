fb78f3d31cf561ffa31339904130b8ed8893ba90
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 06:44:16 -0400

    Update AuthServiceProvider.php

--- Providers/AuthServiceProvider.php
+++ Providers/AuthServiceProvider.php
@@ -6,12 +6,14 @@ use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
+    
 {
     /**
      * The policy mappings for the application.
      *
      * @var array
      */
+    
     protected $policies = [
         'App\Thread' => 'App\Policies\ThreadPolicy',
          'App\Reply' => 'App\Policies\ReplyPolicy',
@@ -23,7 +25,9 @@ class AuthServiceProvider extends ServiceProvider
      * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
      * @return void
      */
+    
     public function boot(GateContract $gate)
+        
     {
         $this->registerPolicies($gate);
 
