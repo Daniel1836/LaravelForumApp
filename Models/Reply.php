718df94b2474dd95886e354d59b81cfc2249bcfc
Daniel1836 <32307419+Daniel1836@users.noreply.github.com>
2020-06-15 06:36:16 -0400

    Update Reply.php

--- Models/Reply.php
+++ Models/Reply.php
@@ -7,25 +7,35 @@ use Illuminate\Database\Eloquent\Model;
 
 
 class Reply extends Model
+     
 {
-     use RecordsActivity;
+       use RecordsActivity;
+     
        protected $guarded = [];
        
        protected $with = ['owner'];
        
     public function getDateFormat()
+         
 {
+         
      return 'Y-m-d H:i:s.u';
+         
 }
+     
  /*
     A reply has a owner
     */
      
-public function owner(){
+public function owner()
+
+{
     return $this->belongsTo(User::class, 'user_id');
 }
 
-public function favorites(){
+public function favorites()
+
+{
    return $this->morphMany(Favorite::class, 'favorited');
 }
      
@@ -33,42 +43,60 @@ public function favorites(){
     Favourite the current reply
     */
      
-public function favorite(){
+public function favorite()
+
+{
     
     $attributes = ['user_id' => auth()->id()];
     
-    if (! $this->favorites()->where($attributes)->exists()){
+    if (! $this->favorites()->where($attributes)->exists())
+    
+    {
+         
 return $this->favorites()->create($attributes);
         
     }
     
-    
 }
 
-public function isFavorited(){
+public function isFavorited()
+
+{
     return !! $this->favorites->where('user_id', auth()->id())->count();
 }
 
-public function getFavoritesCountAttribute(){
+public function getFavoritesCountAttribute()
+
+{
     return $this->favorites->count();
 }
 
-public function thread(){
+public function thread()
+
+{
     return $this->belongsTo(Thread::class);
 }
- public function path(){
+ public function path()
+ 
+ {
      return $this->thread->path() . "#reply-{$this->id}";
  }
     
-    protected static function boot(){
+    protected static function boot()
+    
+    {
         parent::boot();
         
-        static::created(function ($reply) {
+        static::created(function ($reply) 
+                        
+          {
             $reply->thread->increment('replies_count');
-        });
+          });
         
-        static::deleted(function ($reply) {
+        static::deleted(function ($reply) 
+                        
+          {
             $reply->thread->decrement('replies_count');
-        });
+          });
     }
 }
